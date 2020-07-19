871d48eec75fbd49672a6cff0a7e9c978388060a
dushyant <dushyant.sabharwal@gmail.com>
2020-07-19 19:34:44 +0200

    simplify main class

--- src/main/java/org/datadog/monitoring/MonitoringApplication.java
+++ src/main/java/org/datadog/monitoring/MonitoringApplication.java
@@ -2,7 +2,6 @@ package org.datadog.monitoring;
 
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.input.Tailer;
-import org.apache.commons.io.input.TailerListener;
 import org.datadog.monitoring.stats.StatsSummaryWorker;
 import org.datadog.monitoring.logs.ApacheCommonLogsParser;
 import org.datadog.monitoring.logs.LogLine;
@@ -20,95 +19,53 @@ import java.util.concurrent.*;
 
 @Slf4j
 public class MonitoringApplication {
-    /**
-     *
-     */
-    Tailer tailer;
-    /**
-     *
-     */
-    SequentialWorker<String, List<LogLine>> logsWorker;
-    /**
-     *
-     */
-    SequentialWorker<List<LogLine>, StatsSummary> logLinesWorker;
-    /**
-     *
-     */
-    SimpleWorker<StatsSummary> statsSummaryWorker;
-    /**
-     *
-     */
-    SimpleWorker<String> messageWorker;
-
     public static void main(String[] args) {
         Optional<ApplicationConfig> applicationConfigOptional = ApplicationUtil.validateUserProvidedArgs(args);
         if (applicationConfigOptional.isEmpty()) {
             System.exit(1);
         }
 
-        MonitoringApplication  application = new MonitoringApplication();
-
-        application.setupWorkers(applicationConfigOptional.get());
-        application.startWorkers(applicationConfigOptional.get());
+        new MonitoringApplication().startWorkers(applicationConfigOptional.get());
     }
 
-    private void setupWorkers(ApplicationConfig appConfig) {
-        log.trace("setting up workers");
+    private void startWorkers(ApplicationConfig appConfig) {
+        log.trace("starting workers");
 
         BlockingQueue<String> incomingLogsQueue = new LinkedBlockingQueue<>();
         BlockingQueue<List<LogLine>> logLinesQueue = new LinkedBlockingQueue<>();
         BlockingQueue<StatsSummary> statsSummariesQueue = new LinkedBlockingQueue<>();
         BlockingQueue<String> messagesQueue = new LinkedBlockingQueue<>();
 
-        TailerListener listener = new LogsListener(incomingLogsQueue);
+        // These executors will not be shutdown, unless the user closes the application or the main thread dies.
+        ScheduledExecutorService scheduledLogsWorker = Executors.newSingleThreadScheduledExecutor();
+        ExecutorService executableWorkers = Executors.newFixedThreadPool(4);
 
-        // Tail the logs with minimum possible delays so that it doesn't hamper with the rate at which
-        // the consumer is running.
-        tailer = new Tailer(Paths.get(appConfig.getLogFileLocation()).toFile(), listener, 1, true);
-
-        logsWorker = new LogsWorker(incomingLogsQueue, logLinesQueue, new ApacheCommonLogsParser());
-
-        logLinesWorker = new LogLinesWorker(logLinesQueue, statsSummariesQueue, messagesQueue);
-
-        statsSummaryWorker = new StatsSummaryWorker(
-                statsSummariesQueue,
-                messagesQueue,
-                appConfig.getAlertsMonitoringInterval() / appConfig.getStatsDisplayInterval(),
-                appConfig.getRequestsPerSecondThreshold()
-        );
-
-        messageWorker = new PrintMessageWorker(messagesQueue);
-    }
-
-    private void startWorkers(ApplicationConfig appConfig) {
-        Thread logsProducerThread = new Thread(tailer);
-
-        ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
-
-        executorService.scheduleAtFixedRate(logsWorker, appConfig.getStatsDisplayInterval(), appConfig.getStatsDisplayInterval(), TimeUnit.SECONDS);
-
-        Thread statsConsumerThread = new Thread(logLinesWorker);
-
-        Thread alertsThread = new Thread(statsSummaryWorker);
-
-        Thread messagesThread = new Thread(messageWorker);
-
-        log.trace("starting worker threads");
-
-        logsProducerThread.start();
-        statsConsumerThread.start();
-        alertsThread.start();
-        messagesThread.start();
         try {
-            logsProducerThread.join();
-            statsConsumerThread.join();
-            alertsThread.join();
-            messagesThread.join();
-        } catch (InterruptedException e) {
-            log.error("Workers got interrupted", e);
-        }
+            scheduledLogsWorker.scheduleAtFixedRate(
+                    new LogsWorker(incomingLogsQueue, logLinesQueue, new ApacheCommonLogsParser()),
+                    appConfig.getStatsDisplayInterval(),
+                    appConfig.getStatsDisplayInterval(),
+                    TimeUnit.SECONDS
+            );
 
-        executorService.shutdown();
+            // Tail the logs with minimum possible delays so that it doesn't hamper with the rate at which
+            // the consumer is running.
+            executableWorkers.submit(new Tailer(
+                    Paths.get(appConfig.getLogFileLocation()).toFile(),
+                    new LogsListener(incomingLogsQueue),
+                    0,
+                    true)
+            );
+            executableWorkers.submit(new LogLinesWorker(logLinesQueue, statsSummariesQueue, messagesQueue));
+            executableWorkers.submit(new StatsSummaryWorker(
+                    statsSummariesQueue,
+                    messagesQueue,
+                    appConfig.getAlertsMonitoringInterval() / appConfig.getStatsDisplayInterval(),
+                    appConfig.getRequestsPerSecondThreshold()
+            ));
+            executableWorkers.submit(new PrintMessageWorker(messagesQueue));
+        } catch (RejectedExecutionException executionException) {
+            log.error("Could not start one of the workers, run the application again", executionException);
+        }
     }
 }
